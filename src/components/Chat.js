052f58cd9fc15558749cfdc38cc159358999b794
Aviad Benshoshan <Aviad@bensh.com>
2022-04-12 15:04:42 +0300

    added validations to username and password in registration,

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,8 @@
 import '../App.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Link, Route } from 'react-router-dom'
-import { InputGroup, Dropdown, DropdownButton, FormControl, Stack } from 'react-bootstrap';
+import { Dropdown, DropdownButton, Stack } from 'react-bootstrap';
 
 // import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
