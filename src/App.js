1b420e99b5caa97e786fcdf74659c273a8be0677
shirhen194 <shirhanono@gmail.com>
2022-04-13 11:37:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,98 @@ import Register from './components/Register';
 import ChatScreen from './components/ChatScreen';
 import SignIn from './components/SignIn';
 import React from 'react';
+import Conversations from './components/Conversations';
 
 class App extends React.Component {
   constructor() {
     super()
-    this.state ={
-      users: [],
+    this.state = {
+      users: [{ userName: "Shir1", password: "a123", displayName: "Shir", contacts: ["Aviad", "Reut"] },
+      { userName: "Aviad1", password: "a123", displayName: "Aviad", contacts: ["Shir", "Reut"] },
+      { userName: "Reut1", password: "a123", displayName: "Reut", contacts: ["Shir", "Aviad"] }],
+      conversations:[{
+        users:["Shir", "Aviad"],
+         messeages: [
+           {user:"Shir",
+            content: "Hi :)"},
+            {user:"Reut",
+            content: "Hi :)"},
+            {user:"Reut",
+            content: "What's up?"},
+            {user:"Shir",
+            content: "I'm good, doing Pesah homework"},
+            {user:"Shir",
+            content: "You?"},
+            {user:"Reut",
+            content: "I'm great, doing homework too"},
+            {user:"Shir",
+            content: "Good luckk I hope we'll both finish soon"},
+            {user:"Reut",
+            content: "Yeah me too!"},
+          ]},
+          {
+            users:[this.state.users[0], this.state.users[1]],
+             messeages: [
+               {user:"Aviad",
+                content: "Hi Shir,how's it going?"},
+                {user:"Shir",
+                content: "I'm great, how are you?"},
+                {user:"Aviad",
+                content: "Awsome, can you help me with something in github?"},
+                {user:"Shir",
+                content: "Sure! what do you need?"},
+                {user:"Aviad",
+                content: "I'm trying to solve a conflict in merging to dev"},
+                {user:"Shir",
+                content: "Ok, I'll go check it now"},
+              ]}],
     }
   }
 
-  addUser=(user)=>{
-    let newUsers=[...this.state.users, user]
+  componentDidMount() {
+    let NewConversations = [...this.state.conversations]
+    NewConversations.push({
+      users:["Shir", "Aviad"],
+       messeages: [
+         {user:"Shir",
+          content: "Hi :)"},
+          {user:"Reut",
+          content: "Hi :)"},
+          {user:"Reut",
+          content: "What's up?"},
+          {user:"Shir",
+          content: "I'm good, doing Pesah homework"},
+          {user:"Shir",
+          content: "You?"},
+          {user:"Reut",
+          content: "I'm great, doing homework too"},
+          {user:"Shir",
+          content: "Good luckk I hope we'll both finish soon"},
+          {user:"Reut",
+          content: "Yeah me too!"},
+        ]},
+        {
+          users:[this.state.users[0], this.state.users[1]],
+           messeages: [
+             {user:"Aviad",
+              content: "Hi Shir,how's it going?"},
+              {user:"Shir",
+              content: "I'm great, how are you?"},
+              {user:"Aviad",
+              content: "Awsome, can you help me with something in github?"},
+              {user:"Shir",
+              content: "Sure! what do you need?"},
+              {user:"Aviad",
+              content: "I'm trying to solve a conflict in merging to dev"},
+              {user:"Shir",
+              content: "Ok, I'll go check it now"},
+            ]})
+    }
+
+  addUser = (user) => {
+    let newUsers = [...this.state.users, user]
     this.setState({
-      users: newUsers 
+      users: newUsers
     })
   }
 
@@ -26,7 +105,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Routes>
           {/* Routes go here v */}
-          <Route path="/" element={<Register addUser={this.addUser}/>}></Route>
+          <Route path="/" element={<Register addUser={this.addUser} />}></Route>
           <Route path="/chat" element={<ChatScreen />}></Route>
           <Route path="/signIn" element={<SignIn />}></Route>
         </Routes>
