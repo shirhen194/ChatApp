31740217325cfbe6d6ddf6b98b483539ab6c408c
shirhen194 <shirhanono@gmail.com>
2022-04-13 15:39:45 +0300

    delete react-icons

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -5,28 +5,27 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom'
-import { VscSignOut } from "react-icons/vsc";
 
 // import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
 function Conversations(props) {
   return (
     <Container>
-          <Row className='self'>
-            <Col>my pic</Col>
-            <Col xs={4}></Col>
-            <Col>new chat</Col>
-            <Col>
-              <Link to="/signIn">
-                <Button variant="link" type="button" >
-                 <VscSignOut/> Sign Out 
-                </Button>
-              </Link>
-            </Col>
-          </Row>
-          <div>here goes the conversations</div>
-      </Container>
-      
+      <Row className='self'>
+        <Col>my pic</Col>
+        <Col xs={4}></Col>
+        <Col>new chat</Col>
+        <Col>
+          <Link to="/signIn">
+            <Button variant="link" type="button" >
+              Sign Out
+            </Button>
+          </Link>
+        </Col>
+      </Row>
+      <div>here goes the conversations</div>
+    </Container>
+
   );
 }
 
