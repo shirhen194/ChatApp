f0f57453564a011f1a4f34ec1c48f3fabb01a974
shirhen194 <shirhanono@gmail.com>
2022-04-25 11:43:17 +0300

    messages fix

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ class App extends React.Component {
       { userName: "Aviad1", password: "a123", displayName: "Aviad", pic: "cat_aviad.jpg", contacts: ["Shir"] },
       { userName: "Reut1", password: "a123", displayName: "Reut", pic: "cat_reut.jpg", contacts: ["Shir"] }],
       conversations: [{
-        users:["",""],
-        id:0,
-        messages:[]
+        users: ["", ""],
+        id: 0,
+        messages: []
       },
-        {
+      {
         users: ["Shir1", "Reut1"],
         id: 1,
         messages: [
@@ -106,27 +106,36 @@ class App extends React.Component {
 
   componentDidMount = () => {
     this.setState({
-      online: this.state.users[2]
+      online: this.state.users[0]
     })
   }
 
 
   // add message to the array of messages to the right conversation.
-  addMessage = function(message, c_id, type) {
+  addMessage = function (message, c_id, type) {
     // TODO: change c_index to find the right index by c_id
     //TODO: add user information to new message
+    //time and date stamp
+    let timeStamp = new Date().toLocaleTimeString()
+    let timeWithootSeconds = timeStamp.substring(0, timeStamp.length - 3)
+    let dateStamp = new Date().toLocaleDateString()
+    let dateWithootYear = dateStamp.substring(0, dateStamp.length - 5)
     let c_index = c_id;
     let conversations = [...this.state.conversations];
-    let new_message={user: this.state.online.displayName, type:type, content:message}
-    if (c_index !== -1){
+    let new_message = {
+      user: this.state.online.displayName,
+      type: type, content: message,
+      timeStamp: dateWithootYear + " " + timeWithootSeconds
+    }
+    if (c_index !== -1) {
       let updated_conversation = {
         ...conversations[c_index],
         messages: [...conversations[c_index].messages, new_message]
       }
       conversations[c_index] = updated_conversation
-      this.setState({conversations})
+      this.setState({ conversations })
     }
-    else{
+    else {
       console.log("A bug occured! Trying to add a message to an undefined conversation!")
     }
   }
