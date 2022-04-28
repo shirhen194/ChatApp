9b23970e17f9a777fa947e42389c79b093d9f483
shirhen194 <shirhanono@gmail.com>
2022-04-28 23:23:19 +0300

    some more unneccery design

--- src/App.js
+++ src/App.js
@@ -511,7 +511,7 @@ class App extends React.Component {
     //TODO: add user information to new message
     //time and date stamp
     let timeStamp = new Date().toLocaleTimeString()
-    let timeWithootSeconds = timeStamp.substring(0, timeStamp.length - 6)
+    let timeWithootSeconds = timeStamp.substring(0, timeStamp.length - 3)
     let dateStamp = new Date().toLocaleDateString()
     let dateWithootYear = dateStamp.substring(0, dateStamp.length - 5)
     let c_index = c_id;
@@ -519,7 +519,7 @@ class App extends React.Component {
     let new_message = {
       user: this.state.online.displayName,
       type: type, content: message,
-      timeStamp: dateWithootYear + " " + timeWithootSeconds
+      timeStamp: timeWithootSeconds + " " + dateWithootYear
     }
     if (c_index !== -1 && new_message.content !== '') {
       let updated_conversation = {
